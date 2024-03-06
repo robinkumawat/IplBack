ef015b50b605d6c687b41863cef7f1c7aab7aeae
robin kumawat <kumawatrobin7@gmail.com>
2024-03-06 16:11:14 +0530

    connect

---
+++ index.js
@@ -0,0 +1,27 @@
+
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+
+const connection = mongoose.connect(
+  'mongodb+srv://kumawatrobin7:Robin@cluster0.ulve3cr.mongodb.net/ipl?retryWrites=true&w=majority&appName=Cluster0',
+
+);
+
+
+const port = 8000;
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+connection
+  .then(() => {
+    app.listen(port, () => {
+      console.log('Server is started at ' + port);
+    });
+  })
+  .catch((err) => {
+    console.error('There is an ERROR:', err);
+  });
+
