8c60bdf8df50d151fc4f9246e2822a45826c58a7
robin kumawat <kumawatrobin7@gmail.com>
2024-03-22 11:19:49 +0530

    add data

--- Admin/AdminRouter.js
+++ Admin/AdminRouter.js
@@ -1,21 +1,45 @@
-import express  from "express";
+import express from "express";
 import AdminModel from "./AdminModel.js";
-import bcrypt, { hash } from "bcrypt"
+import bcrypt from "bcrypt"
+import Jwt from 'jsonwebtoken'
 
 
 
-const Key = Robinkumawat
+const Key = 'Robinkumawat'
 
-AdminRouter.post("/register",req,res =>{
-    let {name,last,email,password} = req.body 
-    bcrypt.hash(password , 10 ,async (err, hash)=>{
-        if(err){
+const AdminRouter = express.Router()
+
+AdminRouter.post("/register", (req, res) => {
+    let { name, last, email, password } = req.body
+    bcrypt.hash(password, 10, async (err, hash) => {
+        if (err) {
             console.error(err);
             return null
         }
         password = hash
-let admintoRegister = new AdminModel({name,last,email,password})
+        let admintoRegister = new AdminModel({ name, last, email, password })
+        let result = await admintoRegister.save()
+        console.log(result)
+        res.json(result)
     })
+})
+
+AdminRouter.post('/login', async (req, res) => {
+
+    if (req.body.email && req.body.password) {
+        let admintologin = await AdminModel.findOne({ email: req.body.email })
+        bcrypt.compare(req.body.email, admintologin.password, (err, result) => {
+            if (err || !result) {
+                res.status(401).json({ message: "Authentication failed" })
+                console.log(err, result)
+            }
+            else{
+                const paylod = {email:admintologin}
+                const token = Jwt.sing(paylod,Key,{expiresIn:'1h'})
+                res.send(admintologin , token)
+            }
+        })
+    }
 })
 
 
--- index.js
+++ index.js
@@ -2,20 +2,21 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import AdminRouter from './Admin/AdminRouter.js';
 
 const connection = mongoose.connect(
   'mongodb+srv://kumawatrobin7:Robin@cluster0.ulve3cr.mongodb.net/ipl?retryWrites=true&w=majority&appName=Cluster0',
-
 );
 
 
 const port = 8000;
 
 const app = express();
+app.use('./uploads', express.static('uploads'))
 app.use(express.json());
 app.use(cors());
 
-// app.use("/")
+app.use("/user", AdminRouter)
 
 connection
   .then(() => {
